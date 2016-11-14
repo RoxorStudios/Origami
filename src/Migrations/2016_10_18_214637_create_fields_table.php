04ca68ace7640b8ddafa4baf806d8bb8ef8b9feb
Glenn Engelen <glenn@epicbrands.be>
2016-11-14 17:16:46 +0100

    Json to text in migrations

--- src/Migrations/2016_10_18_214637_create_fields_table.php
+++ src/Migrations/2016_10_18_214637_create_fields_table.php
@@ -22,7 +22,7 @@ class CreateFieldsTable extends Migration
             $table->string('type');
             $table->string('description')->nullable();
             $table->boolean('required')->default(false);
-            $table->json('options');
+            $table->text('options')->nullable();
             $table->integer('default')->default(false);
             $table->integer('position');
             $table->index('identifier');
